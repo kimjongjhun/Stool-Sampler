57169db7a051aaae054d56ef6ff7ebc2c64bb738
zlaqh <kim.jongjhun@gmail.com>
2017-01-14 20:17:43 -0500

    update
    update to the poop constructor with auto time and date

--- code/com/PoopApp/Poop.java
+++ code/com/PoopApp/Poop.java
@@ -1,30 +1,56 @@
 package com.PoopApp;
 
-import java.sql.Time.*;
-import java.time.*;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.LocalDateTime.*;
+import java.time.format.DateTimeFormatter;
 
 public class Poop {
+
 	private static int numberOfPoops;
-	private LocalTime t;
-	private Date d;
+
+	private String time;
+	DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+	private String date;
+	DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM/dd/YYYY");
+
 	private String location;
 	private String note;
+	private String color;
+	private int stinkLevel;
+	private int numOfWipes;
+	private String type;
 
-	public Poop() {
-//		this.t =;
-//		this.d =;
-		this.location = "Unknown";
-		this.note = "No note";
+	public Poop(String location, String note, String color, int stinkLevel, int type) {
+		this.time = timeFormat.format(LocalDateTime.now());
+		this.date = dateFormat.format(LocalDateTime.now());
+		this.location = location;
+		this.note = note;
+		this.color = color;
+		this.stinkLevel = stinkLevel;
+		switch (type) {
+		case 1:
+			this.type = "Hard Lumps";
+			break;
+		case 2:
+			this.type = "Lumpy Sausage";
+			break;
+		case 3:
+			this.type = "Cracked Sausage";
+			break;
+		case 4:
+			this.type = "Smooth Snake";
+			break;
+		case 5:
+			this.type = "Soft Blobs";
+			break;
+		case 6:
+			this.type = "Mushy";
+			break;
+		case 7:
+			this.type = "Liquid";
+			break;
+		}
 		numberOfPoops++;
 	}
-	
-	public Poop(LocalTime t, Date d, String l, String n) {
-		this.t = t;
-		this.d = d;
-		this.location = l;
-		this.note = n;
-		numberOfPoops++;
-	}
-	
+
 }
